30d0621217470ff1a88dafa79645e68c36852470
Churilov Dmitry <churilov.dmitrycd@mail.ru>
2023-11-20 12:43:13 +0300

    add variables creation

--- Example_01_HelloConsole/Program.cs
+++ Example_01_HelloConsole/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+int value = 100;
+System.Console.WriteLine(value);
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+System.Console.WriteLine(c);
+System.Console.WriteLine(b);
